bdb5c5b1368f578b06b52783cb6aacd119eb2385
Shittu Ibrahim <shit2wice@gmail.com>
2022-01-29 22:59:15 +0000

    [refactor] Add folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import Country from "./components/Country/Country";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Nav />
+      <Country />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Country/Country.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Country = () => {
+  return <div>This is the Country</div>;
+};
+
+export default Country;
