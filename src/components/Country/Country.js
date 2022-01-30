461e13577dd644e18860a22b841eccd4c8defab2
Shittu Ibrahim <shit2wice@gmail.com>
2022-01-30 21:48:48 +0000

    [feat] Add list of countries to home component

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Country.css";
 
-const Country = () => {
-  return <div>This is the Country</div>;
+const Country = ({ flag, name, population, region, capital }) => {
+  return (
+    <div className="country">
+      <img src={flag} alt="flag" className="country__img" />
+
+      <div className="country__text">
+        <div className="country__text-large">{name}</div>
+        <div className="country__text-small">
+          Population: <span>{population}</span>
+        </div>
+        <div className="country__text-small">
+          Region: <span>{region}</span>
+        </div>
+        <div className="country__text-small">
+          Capital: <span>{capital}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Country;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
+import axios from "axios";
 
 import Nav from "../Nav/Nav";
 import Country from "../Country/Country";
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const api = axios.create({
+    baseURL: "https://restcountries.com/v2/all",
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await api.get("./");
+      setCountries(data.data);
+      return countries;
+    }
+    fetchData();
+  }, [api, countries]);
+
   return (
     <div className="home">
       <Nav />
-      <Country />
+      <div className="home__countries">
+        {countries.map((country) => {
+          return (
+            <Country
+              flag={country.flag}
+              name={country.name}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
