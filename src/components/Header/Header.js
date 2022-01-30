44804bad063858e65628aa56411bf6804bf96886
Shittu Ibrahim <shit2wice@gmail.com>
2022-01-30 00:02:01 +0000

    [style] Add style to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Header.css";
+
+import { IoMoonOutline } from "react-icons/io5";
 
 const Header = () => {
-  return <div>This is the Header</div>;
+  return (
+    <div className="header">
+      <div className="header__left">Where in the world?</div>
+      <div className="header__right">
+        <IoMoonOutline />
+        <div className="header__right__text">Dark Mode</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
