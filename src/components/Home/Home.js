532dd0f721c1786747c4d615ef530aa928728511
Shittu Ibrahim <shit2wice@gmail.com>
2022-01-31 11:51:08 +0000

    [style] Modify list of country in home with response to mobile devices

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,19 +7,34 @@ import Country from "../Country/Country";
 
 const Home = () => {
   const [countries, setCountries] = useState([]);
+  // const [countriesShown, setCountriesShown] = useState([]);
   const api = axios.create({
     baseURL: "https://restcountries.com/v2/all",
   });
 
   useEffect(() => {
+    console.log("entering use effect");
     async function fetchData() {
       const data = await api.get("./");
       setCountries(data.data);
       return countries;
     }
     fetchData();
-  }, [api, countries]);
+  }, []);
 
+  // function getTenRandomCountries() {
+  //   if (countriesShown.length >= 10 || !countries) {
+  //     console.log("countries is empty or too full");
+  //     return;
+  //   }
+  //   for (let i = 0; i < 10; i++) {
+  //     const randomNum = Math.ceil(Math.random() * countries.length);
+  //     setCountriesShown((prevCountriesShown) => [
+  //       ...prevCountriesShown,
+  //       countries[randomNum],
+  //     ]);
+  //   }
+  // }
   return (
     <div className="home">
       <Nav />
